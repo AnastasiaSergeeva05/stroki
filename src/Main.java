19e20bcb7cc9d30de30432cb38298de61398660d
02sergey90 <82273253+02sergey90@users.noreply.github.com>
2022-10-02 19:51:05 +0300

    выполненное дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Домашнее задание 1");
+
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = " Ivanov";
+        String fullName = lastName+" " + firstName+" " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        System.out.println("Домашнее задание 2");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета —"+fullName.toUpperCase());
+
+        System.out.println("Домашнее задание 3");
+
+
+        fullName = "Иванов Семён Семёнович";
+        System.out.println(fullName.replaceAll("ё", "е"));
+
+
     }
 }
